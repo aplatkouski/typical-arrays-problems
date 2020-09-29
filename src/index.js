e5db40e2a5c813070f0642a43eabd6ec1d7f19d1
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2020-09-29 15:20:02 +0300

    feat: implement 'min', 'max' and 'avg' functions
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
+exports.min = function min(array = []) {
+    let minimum = 0;
+    for (let num of array) {
+        if (num < minimum) {
+            minimum = num;
+        }
+    }
+    return minimum;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array = []) {
+    let maximum = 0;
+    for (let num of array) {
+        if (num > maximum) {
+            maximum = num;
+        }
+    }
+    return maximum;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array = []) {
+    let total = 0;
+    for (let num of array) {
+        total += num;
+    }
+    return array.length ? total / array.length : 0;
+};
